ab9f2919e03145297ccb647f06b1f0939551b7e8
Deanna Steers <dsteers@forumone.com>
2026-02-11 08:21:06 -0500

    updating datetime format to comply with requirements

--- src/run.py
+++ src/run.py
@@ -8,10 +8,10 @@ POST_URL = "https://b12.io/apply/submission"
 SIGNING_SECRET = "hello-there-from-b12"
 
 def gh_action_triggered_post_call(github_run_url):
-    print(datetime.datetime.now())
+    print(f"Run Url: {github_run_url}")
     
     payload = {
-        "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "timestamp": datetime.datetime.now().isoformat(),
         "name": "Deanna Steers",
         "email": "deanna.steers@gmail.com",
         "resume_link": "https://deannasteers.com/static/docs/DeannaSteers_CV.pdf",
