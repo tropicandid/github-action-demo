964e70a1d5c2f9a8209d79a1a10ce7b27bc2d207
Deanna Steers <dsteers@forumone.com>
2026-02-11 07:59:54 -0500

    simplifying action args

--- src/run.py
+++ src/run.py
@@ -2,19 +2,20 @@ import requests
 import sys
 import hmac
 import hashlib
+import datetime as datetime
 
 POST_URL = "https://b12.io/apply/submission"
 SIGNING_SECRET = "hello-there-from-b12"
 
-def gh_action_triggered_post_call(timestamp, run_url):
+def gh_action_triggered_post_call(github_run_url):
     
     payload = {
-        "timestamp": timestamp,
+        "timestamp": datetime.datetime.now(),
         "name": "Deanna Steers",
         "email": "deanna.steers@gmail.com",
         "resume_link": "https://deannasteers.com/static/docs/DeannaSteers_CV.pdf",
         "repository_link": "https://github.com/tropicandid/github-action-demo",
-        "action_run_link": run_url
+        "action_run_link": github_run_url
     }
 
     signature = generate_signature_256(str(payload).encode("utf-8"))
@@ -47,11 +48,10 @@ def generate_signature_256(body: bytes) -> str:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: Insufficient arguments provided. Please provide a timestamp and a run URL.")
+    if len(sys.argv) < 2:
+        print("Usage: Insufficient arguments provided. Please provide a run URL.")
         sys.exit(1)
-    
-    timestamp = sys.argv[1]
+
     run_url = sys.argv[2]
-    gh_action_triggered_post_call(timestamp, run_url)
+    gh_action_triggered_post_call(run_url)
 
